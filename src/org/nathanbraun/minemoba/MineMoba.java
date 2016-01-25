f43e7166b82dd80fd70d191bf8c534352a95d026
nathank33 <nathanbraunguitar@gmail.com>
2016-01-25 01:00:38 -0800

    Added path.yml and starter methods

--- src/org/nathanbraun/minemoba/MineMoba.java
+++ src/org/nathanbraun/minemoba/MineMoba.java
@@ -3,5 +3,14 @@ package org.nathanbraun.minemoba;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MineMoba extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		getLogger().info("WOOOO the plugin loaded correctly");
+	}
 
+	@Override
+	public void onDisable() {
+		
+	}
 }
